59f4318ab5a69591a68ac5a699cf1ab2af8b5b0f
Joshua Clark <clarkis117@live.com>
2016-10-14 02:56:06 -0400

    Anagrams and HashTables

---
+++ CrackingtheCodingInterview/HashTablesRansomNote.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace CrackingtheCodingInterview
+{
+	public class HashTablesRansomNote
+	{
+		static void Main(String[] args)
+		{
+			string[] tokens_m = Console.ReadLine().Split(' ');
+			int m = Convert.ToInt32(tokens_m[0]);
+			int n = Convert.ToInt32(tokens_m[1]);
+			string[] magazine = Console.ReadLine().Split(' ');
+			string[] ransom = Console.ReadLine().Split(' ');
+
+			if (magazine.Length < ransom.Length)
+			{
+				Console.WriteLine("No");
+				return;
+			}
+
+			var hashtable = new Dictionary<string, int>();
+
+			for (int i = 0; i < magazine.Length; i++)
+			{
+				var word = magazine[i];
+
+				if (!hashtable.ContainsKey(word))
+				{
+					hashtable.Add(word, 1);
+				}
+				else
+				{
+					hashtable[word]++;
+				}
+			}
+
+			int counter = 0;
+
+			for (int i = 0; i < ransom.Length; i++)
+			{
+				if (hashtable[ransom[i]] >= 1)
+				{
+					counter++;
+
+					hashtable[ransom[i]]--;
+				}
+			}
+
+			Console.WriteLine(counter >= n ? "Yes" : "No");
+		}
+	}
+}
--- CrackingtheCodingInterview/StringsMakingAnagrams.cs
+++ CrackingtheCodingInterview/StringsMakingAnagrams.cs
@@ -13,7 +13,36 @@ namespace CrackingtheCodingInterview
 			string a = Console.ReadLine();
 			string b = Console.ReadLine();
 
+			//counting array since we're dealing with characters a-z
+			int[] counts = new int[26];
+			char letterA = 'a';
 
+			char[] charArray = a.ToCharArray();
+
+			//iterate through a and get the count of each letter in the keyspace a-z
+			for (int i = 0; i < charArray.Length; i++) {
+				counts[charArray[i] - letterA]++;
+			}
+
+			charArray = b.ToCharArray();
+
+			//iterate through b and decrement the count for each letter that occurs in b
+			for (int i = 0; i < charArray.Length; i++) {
+				counts[charArray[i] - letterA]--;
+			}
+
+			//if the count is zero the letters occur in equal freqency in both string a and b
+			//if the count is not zero then those letters need deleted
+
+			int sum = 0;
+
+			//sum all values in counts to get the number of characters that prevent the strings
+			//from being anagrams
+			for (int i = 0; i < counts.Length ; i++){
+				sum += Math.Abs(counts[i]);
+			}
+
+			Console.WriteLine(sum);
 		}
 	}
 }
